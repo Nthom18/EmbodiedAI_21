56cf3c26b0629ad6f6c43fadbd76261032f5aaf2
Nicoline Louise Thomsen <nthom18@student.sdu.dk>
2021-10-01 17:53:24 +0200

    Sensor test complete

--- sensor_test/main.py
+++ sensor_test/main.py
@@ -1,14 +1,58 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
-# Initialize the EV3 brick.
-ev3 = EV3Brick()
-# Initialize a motor at port B.
-test_motor = Motor(Port.B)
-# Play a sound.
-ev3.speaker.beep()
-# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
-test_motor.run_target(500, 90)
-# Play another beep sound.
-ev3.speaker.beep(1000, 500)
+#!/usr/bin/python3
+
+import ev3dev.ev3 as ev3
+from time import sleep
+import signal
+
+
+btn = ev3.Button()
+
+# mA = Motor(Port.A)
+# mB = Motor(Port.B)
+
+mA = ev3.LargeMotor('outA')
+mB = ev3.LargeMotor('outB')
+
+THRESHOLD_LEFT = 30
+THRESHOLD_RIGHT = 350
+
+BASE_SPEED = 30
+TURN_SPEED = 80
+
+# cl = ev3.ColorSensor('in1')
+
+# TouchSensor = ev3.TouchSensor('in3')
+
+# cl.mode = 'COL-COLOR'
+
+# assert cl.connected, "LightSensorLeft(ColorSensor) is noot connected"
+# assert TouchSensor.connected, "Touch sensor is noot connected"
+
+# colors = ('unknown', 'black', 'blue', 'green', 'yellow', 'red', 'white', 'brown')
+
+gy = ev3.GyroSensor('in2')
+gy.mode = 'GYRO-ANG'
+
+us = ev3.UltrasonicSensor('in4')
+us.mode = 'US-DIST-CM'
+
+while True:
+    mA.duty_cycle_sp = BASE_SPEED
+    mB.duty_cycle_sp = BASE_SPEED
+    # tou_val = TouchSensor.value()
+
+    # if tou_val == 1:
+    #     ev3.Sound.beep().wait()
+    #     mA.duty_cycle_sp = 0
+    #     mB.duty_cycle_sp = 0
+    #     # exit()
+    # else: 
+    #     print(colors[cl.value()])
+
+    # dis = us.value() / 10
+    # unit = "cm"
+    # print(str(dis) + " " + unit)
+
+    ang = gy.value()
+    print(str(ang))
+
