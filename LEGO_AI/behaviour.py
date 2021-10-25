6b923da7ebb03c88bd43d05ea104c6d79a09a993
nthom18 <nthom18@student.sdu.dk>
2021-10-25 15:15:06 +0200

    Added state machine in Behaviour script

--- LEGO_AI/behaviour.py
+++ LEGO_AI/behaviour.py
@@ -4,12 +4,68 @@
 import ev3dev.ev3 as ev3
 
 #### CONSTANTS ####
+# Sensors
 REF_VALUE = 35
+COMP_THRESHOLD = 10
+BLACK = 1
+WHITE = 0
+
+# Motors
 BASE_SPEED = 25
 
 
 class Behavior:
-    def __init__(self):
+    def __init__(self, mA, mB):
+
+        self.state = 'init'
+        
+        self.motor_left = mA
+        self.motor_right = mB
+
+
+    def update(self, cl1, cl2):   # State Machine
+        
+        cl1_state = BLACK if cl1 < REF_VALUE else WHITE
+        cl2_state = BLACK if cl2 < REF_VALUE else WHITE
+
+
+        # STATE ACTIONS TODO: Call behaviours
+        match self.state:
+            case 'init':
+                pass
+            
+            case 'solid line':
+                pass
+
+            case 'ghost line':
+                pass
+
+            case _:        
+                pass
+
+
+        # STATE CHANGING
+        match self.state:
+            case 'init':
+                self.state = 'solid line'
+            
+            case 'solid line':
+                if (cl1_state == WHITE and cl2_state == WHITE):
+                    self.state = 'ghost line'
+                else
+                    self.state = 'solid line'
+
+            case 'ghost line':
+                if (cl1_state == BLACK or cl2_state == BLACK):
+                    self.state = 'solid line'
+                else
+                    self.state = 'ghost line'
+
+            case _:        
+                self.state = 'init' # Default case
+
+    
+    def line_follow(self):
         pass
 
 
--- LEGO_AI/main.py
+++ LEGO_AI/main.py
@@ -2,6 +2,8 @@
 
 #### IMPORTS ####
 import ev3dev.ev3 as ev3
+
+from behaviour import Behaviour
 from line_follower import line_follower
 
 
@@ -10,8 +12,10 @@ from line_follower import line_follower
 btn = ev3.Button()
 
 # Colour sensor setup
-cl = ev3.ColorSensor('in1')
-cl.mode = 'COL-REFLECT'
+cl1 = ev3.ColorSensor('in1')
+cl1.mode = 'COL-REFLECT'
+cl2 = ev3.ColorSensor('in2')
+cl2.mode = 'COL-REFLECT'
 
 # Motor Setup
 mA = ev3.LargeMotor('outA')
@@ -19,12 +23,16 @@ mA.run_direct()
 mB = ev3.LargeMotor('outB')
 mB.run_direct()
 
+# Behavior setup
+Egon = Behaviour(mA, mB)
 
 #### PROGRAM LOOP ####
 while True:
 
-    mA.duty_cycle_sp = 50
-    mB.duty_cycle_sp = 50
+    Egon.update(cl1, cl2)
+
+    # mA.duty_cycle_sp = 50
+    # mB.duty_cycle_sp = 50
 
     # line_follower(mA, mB, cl.value())
 
