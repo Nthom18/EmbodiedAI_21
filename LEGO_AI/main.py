d0e5b2dddf3a0d6e2722e6a779fd0adeee2c61c1
nthom18 <nthom18@student.sdu.dk>
2021-11-28 12:52:25 +0100

    Renamed sensors so they are left/right

--- LEGO_AI/main.py
+++ LEGO_AI/main.py
@@ -19,10 +19,10 @@ speaker = Sound()
 btn = ev3.Button()
 
 # Colour sensor setup
-cl1 = ev3.ColorSensor('in1')
-cl1.mode = 'COL-REFLECT'
-cl2 = ev3.ColorSensor('in2')
-cl2.mode = 'COL-REFLECT'
+clL = ev3.ColorSensor('in1')
+clL.mode = 'COL-REFLECT'
+clR = ev3.ColorSensor('in2')
+clR.mode = 'COL-REFLECT'
 
 # Sonic sensor setup
 us = ev3.UltrasonicSensor('in4')
@@ -65,8 +65,8 @@ speaker.beep(1)
 while True:
     
     try:
-        log.log_to_file(t, cl1.value(), cl2.value(), Egon.last_seen)
-        # log.log_to_file(t, cl1.value(), cl2.value(), Egon.control_input, Egon.base_speed)
+        log.log_to_file(t, clL.value(), clR.value(), Egon.last_seen)
+        # log.log_to_file(t, clL.value(), clR.value(), Egon.control_input, Egon.base_speed)
         # log.log_to_file(t, Egon.thrust_left, Egon.thrust_right, Egon.error, Egon.control_input, Egon.base_speed)
         t += 1
 
@@ -84,12 +84,12 @@ while True:
 
 
         # Apply behaviours
-        Egon.update(cl1.value(), cl2.value(), hugged)
+        Egon.update(clL.value(), clR.value(), hugged)
 
         mR.duty_cycle_sp = Egon.thrust_right
         mL.duty_cycle_sp = Egon.thrust_left
         
-        print(t, cl1.value(), cl2.value(), Egon.last_seen)
+        print(t, clL.value(), clR.value(), Egon.last_seen)
  
 
     except KeyboardInterrupt: # CATCHES CTRL+C
