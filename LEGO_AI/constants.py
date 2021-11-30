5fe85e06745895ec2efbcfd2997510de88f7ecef
nthom18 <nthom18@student.sdu.dk>
2021-11-30 09:42:09 +0100

    Photo shoot

--- LEGO_AI/constants.py
+++ LEGO_AI/constants.py
@@ -7,7 +7,7 @@ REF_WHITE = 53
 # Motors
 BASE_SPEED = 30
 
-# Robot Sim Measurements
-R = 55.72 / 2
-L = 117.16 + 22.76
+# Robot Sim Measurements [m]
+R = (55.72 / 2)/1000
+L = (117.16 + 22.76)/1000
 
